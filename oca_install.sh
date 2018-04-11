e96d27fdec5f9e551aa0732473adcf88f91d951d
jeanneths <38205470+jeanneths@users.noreply.github.com>
2018-04-11 10:19:40 +0200

    Update oca_install.sh

--- oca_install.sh
+++ oca_install.sh
@@ -29,7 +29,7 @@ ln -s /opt/odoo/knowledge/attachment_preview /opt/odoo/custom/addons
 ln -s /opt/odoo/knowledge/attachments_to_filesystem /opt/odoo/custom/addons
 ln -s /opt/odoo/knowledge/document_page /opt/odoo/custom/addons
 ln -s /opt/odoo/knowledge/document_url /opt/odoo/custom/addons
-ln -s /opt/odoo/l10n-spain/account_balance_reporting_xls /opt/odoo/custom/addons
+ln -s /opt/odoo/l10n-spain/account_balance_reporting_xlsx /opt/odoo/custom/addons
 ln -s /opt/odoo/l10n-spain/account_balance_reporting /opt/odoo/custom/addons
 ln -s /opt/odoo/l10n-spain/l10n_es_account_asset /opt/odoo/custom/addons
 ln -s /opt/odoo/l10n-spain/l10n_es_account_balance_report /opt/odoo/custom/addons
